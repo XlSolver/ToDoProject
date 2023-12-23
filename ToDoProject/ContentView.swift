6af5aacf2ecb12172f9a08b11c85a63911add8bf
XlSolver <salvatoreflauto0@gmail.com>
2023-12-23 20:29:14 +0100

    First commit

---
+++ ToDoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoProject
+//
+//  Created by Salvatore Flauto on 23/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoProject/ToDoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoProjectApp.swift
+//  ToDoProject
+//
+//  Created by Salvatore Flauto on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
